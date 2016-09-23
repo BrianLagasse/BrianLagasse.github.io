a60b3e3481c61cdc9082f2da8bb9aed4fa2ec869
BrianLagasse <blagasse@yahoo.com>
2016-09-23 09:58:03 -0500

    Update cv.js
    Added jquery dialog

--- scripts/cv.js
+++ scripts/cv.js
@@ -1,9 +1,41 @@
+var BML = BML || {};
+BML.$ = jQuery.noConflict();
 
 cv = (function () {
    "use strict";
 
    var showSection1 =  function() {
-      alert("Section1");
+      if (document.getElementById('dialogBody')) {
+          var elem = document.getElementById('dialogBody');
+          elem.parentNode.removeChild(elem);
+      }
+
+      var _dialogBody = "<div id='dialogBody'><b>Section 1</b><br><br>";
+
+     _dialogBody += "This is section 1 </div>";
+
+      BML.$("#dialog-div").dialog({
+          modal: true,
+          draggable: false,
+          resizable: false,
+          show: 'blind',
+          hide: 'blind',
+          height: 200,
+          width: 300,
+          open: function () {
+             BML.$("#dialog-div").append(_dialogBody );
+          },
+          buttons: [
+
+           {
+               text: "Close",
+               click: function () {
+                   BML.$("#dialog-div").dialog("close");
+
+               }
+           }
+          ]
+      });
    };
 
   return {
